cb1d4702ae8ac67ea9eb1c01763242e46c1cd0ba
Rafiga AGHAZADA <rafiga.aghazada@opensee.io>
2023-06-14 18:48:07 +0200

    Update code

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-# Chemin d'accès complet du fichier CSV
-path = r"data\fr-esr-patrimoine-immobilier-des-operateurs-de-l-enseignement-superieur (3).csv"
-
-# Lire le fichier CSV dans un DataFrame
-data = pd.read_csv(path, sep=";", low_memory=False)
-
+data = pd.read_csv("fr-esr-patrimoine-immobilier-des-operateurs-de-l-enseignement-superieur (3).csv", sep=";", low_memory=False)
 # Afficher les 10 premières lignes du DataFrame dans Streamlit
 st.write(data.head(10))
